ec036e1561f0a3acd1745cd29a695635610f0316
Jakub Němec <jakub333.nemec@gmail.com>
2026-09-25 17:27:10 +0200

    rsi

--- app/indicators.py
+++ app/indicators.py
@@ -5,3 +5,31 @@ from yfinance import data
 def add_ema(data, period):
     data[f"EMA_{period}"] = data["Close"].ewm(span=period, adjust=False).mean()
     return data
+
+
+def add_rsi(data, period=14):
+    delta = data["Close"].diff()
+    gain = delta.where(delta>0, 0)
+    loss = delta.where(delta<0, 0).abs()
+
+    avg_gain = gain.ewm(alpha=1/period, adjust=False).mean()
+    avg_loss = loss.ewm(alpha=1/period, adjust=False).mean()
+    rs = avg_gain / avg_loss
+
+    data[f"RSI_{period}"] = 100 - (100 / (1 + rs))
+
+    return data
+
+def add_atr(data, period=14):
+    previous_close = data["Close"].shift(1)
+
+    high_low = data["High"] - data["Low"]
+    high_close = data["High"] - previous_close.abs()
+    low_close = data["Low"] - previous_close.abs()
+
+    tr = pd.concat([high_low, high_close, low_close], axis=1).max(axis=1)
+    atr = tr.ewm(alpha=1/period, adjust=False).mean()
+    
+    data[f"ATR_{period}"] = atr
+    
+    return data
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from app.market_data import get_market_data
-from app.indicators import add_ema
+from app.indicators import add_ema, add_rsi, add_atr
+
 
 def main():
     data = get_market_data("QQQ")
@@ -8,8 +9,10 @@ def main():
     data = add_ema(data, 50)
     data = add_ema(data, 200)
 
+    data = add_rsi(data, 14)
+    data = add_atr(data, 14)
 
-    print(data[["Close", "EMA_20", "EMA_50", "EMA_200"]].tail(10))
+    print(data[["Close", "EMA_20", "EMA_50", "EMA_200", "RSI_14", "ATR_14"]].tail(10))
 
 
 if __name__== "__main__":
